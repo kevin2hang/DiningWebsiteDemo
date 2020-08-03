eb4ac2feb79b00ae54daaa9dbe45873af830c750
Kevin Zhang <56693266+kevin2hang@users.noreply.github.com>
2020-08-02 23:04:42 -0700

    Initial commit of Menu.

---
+++ Menu.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Kevin's Fine Dining/Menu</title>
+    <style>
+        body{
+        font-family: Cambria, Arial, sans-serif;
+        }
+        #title{
+        text-align: center;
+        margin-top: 40px;
+        }
+         #main {
+        margin-left: auto;
+        margin-right: auto;
+        width: 1200px;
+        padding: 10px;
+        border-style : groove;
+        border-width: 3px;
+        }
+        #Navigation{
+        background-color: black;
+        position: fixed;
+        overflow: hidden;
+        top: 0;
+        width: 100%;
+        }
+        a{
+        color: white;
+        text-decoration: none;
+        display:block;
+        padding: 14px 16px;
+        float: left;
+        }
+        h2, h1, h3, h6{
+        margin: 0;
+        }
+        li{
+        list-style-type: none;
+        margin-left: 15px;
+        }
+        a:hover {
+        color: gold;
+        }
+
+
+
+
+    </style>
+</head>
+<body background="images/EiffelTower.jpg">
+
+
+<div id="Navigation"><font color="white">
+    <a href="index.php" alt="Home">Home</a>
+    <a href="Menu.php" alt="Menu">Menu</a>
+    <a href="Reservations.php" alt="Reservations" >Reservations</a>
+    <a href="Photos.php" alt="Photos">Photos</a>
+    <a href="Review.php" alt="Reviews">Reviews</a></font>
+</div>
+<div id="title"><br> <strong><font color="gold"><h2>Kevin's Fine Dining</h2></font> </strong><br></div>
+
+    <div id="main">
+        <font color="white"><h2>Menu</h2><br>
+            <h3>Burger's</h3>
+            <li>Double Bacon CheeseBurger with a Side of Fries $11.99</li>
+            <li>Jalapeno Chicken Burger $11.39</li>
+            <h3>Pasta's</h3>
+            <li>Pasta with Chicken Alfredo Sauce $13.99</li>
+            <li>Spaghetti & Meatballs with Marinara Sauce $14.49</li>
+            <h3>Pizza</h3>
+            <li>Italian Pepperoni Pizza with Basil Leaves $13.99</li>
+            <li>Mozzarella Cheese Pizza $10.99</li>
+            <h3>Sandwiches</h3>
+            <li>Philly Cheese Steak with a Side of Fries $14.99</li>
+            <li>Grilled Cheese Sandwich with 3 Cheeses $8.99<h6>(Choose 3: Provolone, Swiss, Pepper Jack, Mozzarella, Cheddar, & Havarti)</h6></li>
+            <h3>Specialty's</h3>
+            <li>Roasted Chicken with Special House Sauce $10.99</li>
+
+        </font>
+
+    </div>
+
+
+
+</body>
+</html>
