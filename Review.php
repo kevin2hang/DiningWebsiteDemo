fe6eccc9fdf48fbc0ca5f5599ea5c5bd57cbe73c
Kevin Zhang <56693266+kevin2hang@users.noreply.github.com>
2020-08-02 23:06:36 -0700

    Initial commit of Review.

---
+++ Review.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Kevin's Fine Dining/Reviews</title>
+    <style>
+        body{
+        font-family: Cambria, Arial, sans-serif;
+        }
+        #title{
+        text-align: center;
+        margin-top: 40px;
+        }
+        #main {
+        margin-left: auto;
+        margin-right: auto;
+        width: 1200px;
+        padding: 10px;
+        border-style : groove;
+        border-width: 3px;
+        }
+        #Navigation{
+        background-color:black;
+        position: fixed;
+        top: 0;
+        overflow: hidden;
+        width: 100%;
+        }
+    a{
+    color: white;
+    text-decoration: none;
+    display: block;
+    margin:0;
+    padding: 14px 16px;
+    float: left;
+    }
+    h2, h1,h3{
+    margin: 0;
+    }
+
+    a:hover {
+    color: gold;
+    }
+
+
+
+
+    </style>
+</head>
+<body background="images/EiffelTower.jpg">
+<div id="Navigation"><font color="white">
+    <a href="index.php" alt="Home">Home</a>
+    <a href="Menu.php" alt="Menu">Menu</a>
+    <a href="Reservations.php" alt="Reservations" >Reservations</a>
+    <a href="Photos.php" alt="Photos">Photos</a>
+    <a href="Review.php" alt="Reviews">Reviews</a></font>
+</div>
+
+<div id="title"><br> <strong><font color="gold"><h2>Kevin's Fine Dining</h2></font> </strong><br></div>
+
+    <div id="main">
+        <font color="white"><h2>Reviews</h2><br>
+            Anonymous<p>I came here and got the double bacon cheeseburger. It was really delicous and filling. Definitely
+                got more than I payed for. =)</p></font>
+
+    </div>
+
+
+
+</body>
+</html>
