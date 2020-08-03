d65edc6600c60fb7282202790bc4c4a32c186597
Kevin Zhang <56693266+kevin2hang@users.noreply.github.com>
2020-08-02 23:03:39 -0700

    Initial commit of Index.

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Kevin's Fine Dining</title>
+    <style>
+        body{
+        font-family: Cambria, Arial, sans-serif;
+        }
+        #title{
+        text-align: center;
+        margin-top: 40px;
+        }
+
+        #main {
+        margin-left: auto;
+        margin-right: auto;
+        width: 1200px;
+        padding: 10px;
+        border-style : groove;
+        border-width: 3px;
+        }
+        #Navigation{
+        background-color: black;
+        position: fixed;
+        overflow: hidden;
+        top: 0;
+        width: 100%;
+        }
+         a{
+          color: white;
+          text-decoration: none;
+          display: block;
+          margin: 0;
+          padding: 14px 16px;
+          float: left;
+          }
+         h2, h1, h3{
+        margin: 0;
+        }
+
+        a:hover {
+        color: gold;
+        }
+
+
+
+
+
+
+    </style>
+</head>
+<body background="images/EiffelTower.jpg">
+
+<div id="Navigation">
+        <a href="index.php" alt="Home">Home</a>
+        <a href="Menu.php" alt="Menu">Menu</a>
+        <a href="Reservations.php" alt="Reservations">Reservations</a>
+        <a href="Photos.php" alt="Photos">Photos</a>
+        <a href="Review.php" alt="Reviews">Reviews</a></font>
+    </font>
+</div>
+
+<div id="title"><br> <strong><font color="gold"><h2>Kevin's Fine Dining</h2></font> </strong><br></div>
+
+
+    <div id="main">
+        <font color="white"><h2>Home</h2><br>
+            Welcome to Kevin's Fine Dining.
+            <p>
+                We offer a variety of food from burgers to pasta to sandwiches to steak to pizza to sandwiches. All our
+                food is made from scratch with fresh ingredients and on the spot. Rest assured knowing that we highly
+                value quality.<br>
+                We currently only have one location located in Paris, France.
+            </p>
+        </font>
+    </div>
+
+
+
+</body>
+</html>
