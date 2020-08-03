17a5decc401190b5350310dd514c1cdbce8db15e
Kevin Zhang <56693266+kevin2hang@users.noreply.github.com>
2020-08-02 23:05:11 -0700

    Initial commit of Photos

---
+++ Photos.php
@@ -0,0 +1,87 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Kevin's Fine Dining/Photos</title>
+    <style>
+        body{
+        font-family: Cambria, Arial, sans-serif;
+        }
+        #title{
+        text-align: center;
+        margin-top: 40px;
+        }
+         #main {
+        margin-left: auto;
+        margin-right: auto;
+        width: 1200px;
+        border-style: groove;
+        border-width: 3px;
+        padding: 10px;
+        }
+        #Navigation{
+        position: fixed;
+        top: 0;
+        overflow: hidden;
+        width: 100%;
+        background-color: black;
+        }
+    a{
+    color: white;
+    text-decoration: none;
+    float: left;
+    display: block;
+    padding: 14px 16px;
+    margin: 0;
+    }
+    h2, h1,h3{
+    margin: 0;
+    }
+
+    a:hover {
+    color: gold;
+    }
+
+    #col1{
+    float: left;
+    width: 560;
+    padding: 10px;
+    }
+    #col2{
+    float: right;
+    width: 560;
+    padding: 10px;
+    }
+
+
+
+    </style>
+</head>
+<body background="images/EiffelTower.jpg">
+<div id="Navigation"><font color="white">
+    <a href="index.php" alt="Home">Home</a>
+    <a href="Menu.php" alt="Menu">Menu</a>
+    <a href="Reservations.php" alt="Reservations" >Reservations</a>
+    <a href="Photos.php" alt="Photos">Photos</a>
+    <a href="Review.php" alt="Reviews">Reviews</a></font>
+</div>
+
+<div id="title"><br> <strong><font color="gold"><h2>Kevin's Fine Dining</h2></font> </strong><br></div>
+
+<div id="main">
+    <font color="white"><h2>Photos</h2>
+<!--        <div id="col1">-->
+            <p><img src="Images/PhillyCheeseSteak.jpg" alt="Philly Cheese Steak" width="540"><img src="Images/DoubleBaconCheeseBurger.jpg" alt="Double Bacon CheeseBurger" width="540"></p>
+<!--        </div>-->
+
+<!--        <div id="col2">-->
+            <p><img src="Images/CheesePizza.jpg" alt="Cheese Pizza" width = "540"></p>
+<!--        </div>-->
+
+    </font>
+
+</div>
+
+
+</body>
+</html>
